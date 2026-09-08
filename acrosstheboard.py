43118fa0b09ee08ab2bb340bc75942acb134519d
eva <Ehite2033@norfolkcollegiate.org>
2026-09-08 16:05:11 -0400

    Refactor and implement Run method in acrosstheboard.py; clean up comments in eva1.py and remove unused code in aarav1.py

--- acrosstheboard.py
+++ acrosstheboard.py
@@ -5,22 +5,21 @@ from base_robot import *
 # Simple test mission: wait for the forward button, then drive forward,
 # turn, and drive again. This gives you a predictable pattern for tuning.
 # When we run this program from the master program, we will call this
-# "Run(br)" method.
 def Run(br: BaseRobot):
-    # Wait until the team is ready and the robot is in its starting position.
     br.driveForDistance(
-        distance=650,
-        speedPct=100,
+        distance=800,
+        speedPct=80,
         then=Stop.BRAKE,
         waiting=True,
     )
+    br.turnInPlace(angle=90, speedPct=45)
     br.driveForDistance(
-        distance=-500, speedPct=100, then=Stop.BRAKE, waiting=True
+        distance=1000, speedPct=80, then=Stop.BRAKE, waiting=True
     )
 
 
 # Leave everything below here and don't type anything below this line
-# If running this program directly (not from the master program), this is+
+# If running this program directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
 # create a BaseRobot and run the Run(br) method above.
 # In other words, keep these three lines at the bottom of your code and
